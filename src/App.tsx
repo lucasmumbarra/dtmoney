99baa3281c83b97ed652b7bc79eb37c54bdc5dff
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-04-01 12:17:20 -0300

    Consumo da API, e utilização dos dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
+import { NewTransactionModal } from './Components/NewTransactionModal';
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
